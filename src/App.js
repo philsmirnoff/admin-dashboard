76af3c536fcc4dc4a2d896555821f7a7b807fd03
Ilfir Yakupov <philsmirnoff@gmail.com>
2025-01-23 15:20:50 -0500

    add calendar

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Form from "./scenes/form";
 // import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
-// import Calendar from "./scenes/calendar/calendar";
+import Calendar from "./scenes/calendar";
 
 
 
@@ -37,11 +37,11 @@ function App() {
               <Route path="/contacts" element={<Contacts />} />
              <Route path="/invoices" element={<Invoices />} />
             <Route path="/form" element={<Form />} />
+            <Route path="/calendar" element={<Calendar />} />
             {/*     <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
               <Route path="/faq" element={<FAQ />} />
-              <Route path="/calendar" element={<Calendar />} />
               <Route path="/geography" element={<Geography />} />  */}
             </Routes>
       </main>
