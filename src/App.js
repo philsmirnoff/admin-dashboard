9dc4e0b17f06d76ac1101c0a086a2c1f79fb4915
Ilfir Yakupov <philsmirnoff@gmail.com>
2025-01-22 20:38:15 -0500

    set up theme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { ColorModeContext, useMode  } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
+
 
 function App() {
+  const [theme, colorMode] = useMode();
+
+
   return (
+    <ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
     <div className="app">
-      <h1>Hello World!</h1>
+      <main className="content"></main>
     </div>
+    </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
