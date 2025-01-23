3e55f5434c3403ba32d4fc534ec52e47b085c5f3
Ilfir Yakupov <philsmirnoff@gmail.com>
2025-01-23 15:30:17 -0500

    add BarChart

--- src/App.js
+++ src/App.js
@@ -8,13 +8,14 @@ import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
 import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
-// import Bar from "./scenes/bar";
+import FAQ from "./scenes/faq";
 import Form from "./scenes/form";
+import Calendar from "./scenes/calendar";
+// import Geography from "./scenes/geography";
+// import Bar from "./scenes/bar";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
-// import FAQ from "./scenes/faq";
-// import Geography from "./scenes/geography";
-import Calendar from "./scenes/calendar";
+
 
 
 
@@ -35,13 +36,13 @@ function App() {
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
-             <Route path="/invoices" element={<Invoices />} />
-            <Route path="/form" element={<Form />} />
-            <Route path="/calendar" element={<Calendar />} />
-            {/*     <Route path="/bar" element={<Bar />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/faq" element={<FAQ />} />
+         {/*  <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
-              <Route path="/faq" element={<FAQ />} />
               <Route path="/geography" element={<Geography />} />  */}
             </Routes>
       </main>
